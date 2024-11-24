cd0ba493cd9d462c19105d9ce4a43270b67edbfe
MuzanKiButsu <145572500+MuzanKiButsu@users.noreply.github.com>
2024-11-24 20:40:06 +0530

    Update admin_panel.py

--- plugins/admin_panel.py
+++ plugins/admin_panel.py
@@ -36,8 +36,8 @@ async def tutorial(bot,message):
 	    text =Txt.FILE_NAME_TXT.format(format_template=format_template),
 	    disable_web_page_preview=True,
 	    reply_markup=InlineKeyboardMarkup([
-        			[InlineKeyboardButton("🦋 Admin",url = "https://t.me/Sakazuki_Akainu_l"), 
-        			InlineKeyboardButton("⚡ Tutorial",url = "https://t.me/SelfProclaimed_Dev") ]])
+        			[InlineKeyboardButton("🦋 Admin",url = "https://t.me/STRONGEST_MARINE"), 
+        			InlineKeyboardButton("⚡ Tutorial",url = "https://t.me/+YRXdcZyShNpiZDY1") ]])
 	)
 
 
